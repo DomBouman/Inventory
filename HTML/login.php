971403f17374cfa1a5dcd11eff72b215a527dd0d
Dominik Bouman <Dwbouman161@stevenscollege.edu>
2020-04-03 09:39:22 -0400

    Added some folders and started html and css

---
+++ HTML/login.php
@@ -0,0 +1,19 @@
+<?php
+include_once "../db.php";
+?>
+
+<?php
+session_start()
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <link rel="stylesheet" href="../css/main.css">
+    <title>Login</title>
+  </head>
+  <body>
+
+  </body>
+</html>
